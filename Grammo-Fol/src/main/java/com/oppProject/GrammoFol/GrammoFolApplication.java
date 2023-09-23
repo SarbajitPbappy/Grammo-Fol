5a37270972483d743937da05a7c01f71ac2c6b6e
Bappy6155 <105677054+Bappy6155@users.noreply.github.com>
2023-09-23 13:03:34 +0600

    Project

---
+++ Grammo-Fol/src/main/java/com/oppProject/GrammoFol/GrammoFolApplication.java
@@ -0,0 +1,13 @@
+package com.oppProject.GrammoFol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrammoFolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrammoFolApplication.class, args);
+	}
+
+}
